ce72a82e5ce03e63cf8dd27a747f834099b7bd6d
vijendrabhandarkar <--global>
2021-11-03 18:02:53 +0530

    nodejs

--- readData.js
+++ readData.js
@@ -2,19 +2,27 @@ const http=require("http")
 const fs=require("fs")
 
 http.createServer((req,res)=>{
-    fs.readFile("./asyncview/view.txt",(err,data)=>{
-        if (err) {
-            res.writeHead(404,{
-                'content-type':'text/plain'
-            })
-            res.write("File not found")
-            res.end()
-        } else {
-            res.writeHead(200,{
-                'content-type':'text/html'
-            })
-            res.write(data)
-            res.end()
-        }
-    })
+    if (req.url==='/'&&req.method==="GET") {
+        fs.readFile("./asyncview/view.txt",(err,data)=>{
+            if (err) {
+                res.writeHead(404,{
+                    'content-type':'text/plain'
+                })
+                res.write("File not found")
+                res.end()
+            } else {
+                res.writeHead(200,{
+                    'content-type':'text/html'
+                })
+                res.write(data)
+                res.end()
+            }
+        })
+    } else {
+        res.writeHead(404,{
+            'content-type':'text/html' 
+        })
+        res.end("Invalied path or invalied HTTP method")
+    }
+    
 }).listen(2000)
